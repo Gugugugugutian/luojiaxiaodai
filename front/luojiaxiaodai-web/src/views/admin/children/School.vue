fbacd0198a3d1e8f5d05a125cac344b2b6381db9
henryzhuhr <henry.zhuhr@icloud.com>
2024-10-03 17:13:10 +0800

    🐛 fix: 修复 quill 缺失文件导致无法正确启动的问题

--- front/luojiaxiaodai-web/src/views/admin/children/School.vue
+++ front/luojiaxiaodai-web/src/views/admin/children/School.vue
@@ -118,7 +118,7 @@
 
             /** 提交按钮 */
             submit() {
-                console.log(this.value.children.[0])
+                console.log(this.value.children[0])
             this.$refs["form"].validate(valid => {
                 if (valid) {
                     if (this.form.id != null) {
@@ -139,12 +139,12 @@
                             }).then(response => {
                                 const name = this.form.deptname;
                                 for(let i=0 ;i<this.value.children.length;i++){
-                                    if(this.value.children.[i].deptname==name){
+                                    if(this.value.children[i].deptname==name){
                                         this.$message("新增专业成功");
                                         this.newList();
                                         this.$post("/class",{
                                         "schoolId":this.value.schoolId,
-                                        "deptId":this.value.children.[i].deptsId,
+                                        "deptId":this.value.children[i].deptsId,
                                         "name": this.form.classesname,
                                         }).then(response => {
                                             this.$message("新增班级成功");
